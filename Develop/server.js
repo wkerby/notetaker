a6785495a76c7e2148bebdb15d8b5e469064aa5e
William Kerby <w.kerby99@gmail.com>
2023-03-19 13:24:56 -0400

    begin DELETE route

--- Develop/server.js
+++ Develop/server.js
@@ -30,12 +30,12 @@ app.listen(PORT, () => {
 
 //create GET /api/notes endpoint that returns current notes data in json
 app.get('/api/notes', (req, res) => {
-    const data = JSON.parse(fs.readFileSync('./db/db.json')); //reads the db.json file updated when POST api route is called
+    let data = JSON.parse(fs.readFileSync('./db/db.json')); //reads the db.json file updated when POST api route is called
     res.json(data);
 });
 
 app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add a review`);
+    console.info(`${req.method} request received to add a note`);
     console.log(req.body);
 
     // Destructuring assignment for the items in req.body
@@ -82,5 +82,30 @@ app.post('/api/notes', (req, res) => {
     }
 });
 
+//create DELETE route
+app.delete('/api/notes/:id', (req, res) => {
+    console.info(`${req.method} request received to delete a note`);
+    const queryId = req.params.id;
+    console.log(queryId);
+    // if (queryId) {
+    //     let data = JSON.parse(fs.readFileSync('./db/db.json'));
+    //     for (let i = 0; i < data.length; i++) {
+    //         if (data[i].id == queryId) {
+    //             try {
+    //                 data.splice(i, 0, "{}");
+    //             }
+    //             catch {
+    //                 console.log("There was an error deleting note from JSON file")
+    //             }
+
+    //             fs.writeFileSync(`./db/db.json`, JSON.stringify(data, null, 4), (err) => {
+    //                 err
+    //                     ? console.error(err)
+    //                     : console.info("Successfully updated notes!")
+    //             });
+    //         }
+    //     }
+    // }
+});
 
 
