73c156b70b158298e53628804870d7e9a16b4bbc
William Kerby <w.kerby99@gmail.com>
2023-03-19 09:00:18 -0400

    allow server to handle JSON data

--- Develop/server.js
+++ Develop/server.js
@@ -10,6 +10,10 @@ const PORT = 3001;
 //middleware that allows us to use all files located in public directory
 app.use(express.static('public'));
 
+//allow server to work with JSON data
+app.use(express.json())
+
+
 //set up GET route for home page
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
@@ -29,6 +33,7 @@ app.get('/api/notes', (req, res) => res.json(db));
 
 app.post('/api/notes', (req, res) => {
     console.info(`${req.method} request received to add a review`);
+    console.log(req.body);
 
     // Destructuring assignment for the items in req.body
     const { title, text } = req.body;
